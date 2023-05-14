bebd1a6f484b292bf118201b5c837da8b1282644
DimaLedentsov <ledentd@mail.ru>
2023-05-14 21:38:42 +0300

    done

--- old/interpolationold.py
+++ old/interpolationold.py
@@ -1,3 +1,4 @@
+
 from math import *
 
 #менять тута
@@ -6,9 +7,9 @@ data = \
 0,1 0,2 0,3 0,4 0,5
 1,25 2,38 3,79 5,44 7,14
 '''
-x0 = 0.28
+x0 = 0.22
 #выбрать нужный после lambda
-get_method = lambda: nwt
+get_method = lambda: nwtn
 #x = "1.1 2.3 3.7 4.5 5.4 6.8 7.5"
 #y = "2.73 5.12 7.74 8.91 10.59 12.75 13.43"
 data = data.replace(",",'.')
@@ -24,26 +25,28 @@ print(X)
 print(Y)
 print("решение:")
 
+
 dots = [(X[i],Y[i]) for i in range(N)]
-def lag(dots, x):
+
+def lag(X, Y, x):
     """ Многочлен Лагранжа """
     result = 0
 
-    n = len(dots)
+    n = len(X)
     for i in range(n):
         c1 = c2 = 1
         for j in range(n):
             if i != j:
-                c1 *= x - dots[j][0]
-                c2 *= dots[i][0] - dots[j][0]
-        l = dots[i][1] * c1 / c2
-        print(f"l_{i} = {c1:.6f}/{c2:.6f}*{dots[i][1]}={l:.6f}")
+                c1 *= x - X[j]
+                c2 *= X[i] - X[j]
+        l = Y[i] * c1 / c2
+        print(f"l_{i} = {c1:.6f}/{c2:.6f}*{Y[i]}={l:.6f}")
         result += l
 
     return result
 
 
-def t_calc_nwt(t, n, forward=True):
+def t_calc(t, n, forward=True):
     """ Вычислить параметр 't' для многочлена Ньютона """
     result = t
 
@@ -52,18 +55,6 @@ def t_calc_nwt(t, n, forward=True):
             result *= t - i
         else:
             result *= t + i
-
-    return result
-
-def t_calc_gau(t, n, forward=True):
-    """ Вычислить параметр 't' для многочлена Гаусса """
-    result = t
-
-    for i in range(1, n):
-        if forward:
-            result *= t - i
-        else:
-            result *= t + (-1)**i*i
 
     return result
 
@@ -113,14 +104,14 @@ def nwt(dots, x):
 
         result = a[x0][0]
         for i in range(1, n):
-            result += (t_calc_nwt(t, i) * a[x0][i]) / factorial(i)
+            result += (t_calc(t, i) * a[x0][i]) / factorial(i)
     else:
         # Вторая интерполяционная формула Ньютона
         t = (x - dots[n - 1][0]) / h
 
         result = a[n - 1][0]
         for i in range(1, n):
-            result += (t_calc_nwt(t, i, False) * a[n - i - 1][i]) / factorial(i)
+            result += (t_calc(t, i, False) * a[n - i - 1][i]) / factorial(i)
 
     return result
 
@@ -146,7 +137,7 @@ def gau(dots, x):
     fmt_table(table)
     #####################
     if x <= dots[n // 2][0]:
-        # Первая интерполяционная формула Гаусса
+        # Первая интерполяционная формула Ньютона
         x0 = n - 1
         for i in range(n):
             if x <= dots[i][0]:
@@ -154,21 +145,66 @@ def gau(dots, x):
                 break
         if x0 < 0:
             x0 = 0
-     
-        t = (x - dots[int(n / 2)][0]) / h
+        t = (x - dots[x0][0]) / h
+
         result = a[x0][0]
         for i in range(1, n):
-            
-            result += (t_calc_gau(t, i) * a[int((n - i) / 2) - (n % 2 == 0)][i]) / factorial(i)
+            result += (t_calc(t, i) * a[x0][i]) / factorial(i)
     else:
-        # Вторая интерполяционная формула Гаусса
+        # Вторая интерполяционная формула Ньютона
         t = (x - dots[n - 1][0]) / h
 
         result = a[n - 1][0]
         for i in range(1, n):
-            
-            result += (t_calc_gau(t, i, False) * a[int((n - i) / 2)][i]) / factorial(i)
+            result += (t_calc(t, i, False) * a[n - i - 1][i]) / factorial(i)
 
     return result
 
-print(get_method()(dots, x0))
+def fin_dif(Y):
+    n = len(Y)
+    matrix =  [[0] * n for _ in range(n)]
+    for i in range(len(Y)):
+        matrix[i][0] = Y[i]
+    for j in range(len(Y)):
+        for i in range(len(Y)):
+            if i + j <= (int(len(Y)) - 1) and j != 0:
+                matrix[i][j] = matrix[i + 1][j - 1] - matrix[i][j - 1]
+    fmt_table(matrix)
+    return matrix
+def gaun( X, Y, x):
+    summ = 0
+    matrix = fin_dif(Y)
+    if x <= X[len(X) // 2]:
+        factor = 1
+        t = (x - X[int(len(X) / 2)]) / (X[1] - X[0])
+        
+        for i in range(len(X)):
+            summ = summ + matrix[int((len(X) - i) / 2) - (len(X) % 2 == 0)][i] * factor / factorial(i)
+            factor = factor * (t - i)
+    else:
+        factor = 1
+        t = (x - X[int(len(X) / 2)]) / (X[1] - X[0])
+        for i in range(len(X)):
+            summ = summ + matrix[int((len(X) - i) / 2)][i] * factor / factorial(i)
+            factor = factor * (t + (-1) ** i * i)
+    return summ
+
+def nwtn( X, Y, x):
+    summ = 0
+    matrix = fin_dif(Y)
+    n = len(X)
+    if x <= X[n // 2]:
+        t = (x - X[0]) / (X[1] - X[0])
+        factor = 1
+        for i in range(len(X)):
+            summ = summ + matrix[0][i] * factor / factorial(i)
+            factor = factor * (t - i)
+    else:
+        t = (x - X[len(X) - 1]) / (X[1] - X[0])
+        factor = 1
+        for i in range(len(X)):
+            summ = summ + matrix[len(X) - 1 - i][i] * factor / factorial(i)
+            factor = factor * (t + i)
+    return summ
+print(get_method()(X,Y, x0))
+print(nwt(dots,x0))
